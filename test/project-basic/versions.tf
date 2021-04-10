d2506f74a86b4666fc56939e7ae2c2194fffef16
Luis Mayta <luis@hadenlabs.com>
2021-04-10 09:04:22 -0500

    fix: providers for test (#5)

--- test/project-basic/versions.tf
+++ test/project-basic/versions.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = ">=0.13.0"
+  required_version = ">= 0.13"
+
   required_providers {
-
-    github = {
-      source  = "integrations/github"
-      version = ">=4.5.0"
+    sentry = {
+      source  = "jianyuan/sentry"
+      version = "~> 0.6.0"
     }
-
   }
 }
