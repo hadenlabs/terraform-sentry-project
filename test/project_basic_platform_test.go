aac00aac5dc99308b87b8d53237f1ee3ca1da550
Luis Mayta <luis@hadenlabs.com>
2021-06-20 13:47:34 -0500

    style: var name format golangci (#8)

--- test/project_basic_platform_test.go
+++ test/project_basic_platform_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestProjectPlatformBasicSuccess(t *testing.T) {
-
 	organizationName := "me-urs"
 	teamName := "me"
 
@@ -43,7 +42,7 @@ func TestProjectPlatformBasicSuccess(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-    t.Parallel()
+		t.Parallel()
 		t.Run(tt.name, func(t *testing.T) {
 			terraformOptions := &terraform.Options{
 				// The path to where your Terraform code is located
